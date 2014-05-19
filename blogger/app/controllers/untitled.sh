db57dfba9b39f489b6c818b4b75faf33dab817eb
Albert Carter <albert.r.carter@gmail.com>
2014-05-19 17:27:32 -0400

    article routes partially complete

---
+++ blogger/app/controllers/untitled.sh
@@ -0,0 +1,14 @@
+bin/rails generate model Article
+	-> change db/migrate/(time_stamp)_create_articles.rb to include fields
+
+bin/rake db:migrate
+bin/rails console
+	-> add articles.
+
+bin/rake routes #sets up routes based on the model.
+
+bin/rails generate controller articles # generates controller file
+	-> edit app/controllers/article_controller.rb to add an index method
+
+touch app/views/articles/index.html.erb
+	-> add some markup 
---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '90e4506abd8533b1670d080896ec970d2c2181356de107f64d5be5c65cdd33642d00cbd9a3bb634da7e31710ba77318aea0f39f6c6973974f7619eb485e03078'
