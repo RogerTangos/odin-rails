fb80881ab0925e810f62b27a61f10c87791631ac
Albert Carter <albert.r.carter@gmail.com>
2014-05-20 16:03:06 -0400

    complete form-based workflow feature

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,8 +1,35 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
 	def show
 		@article = Article.find(params[:id])
+	end
+	def new
+		@article = Article.new
+	end
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "article'#{@article.title}' Created"
+		redirect_to article_path(@article)
+	end
+	def destroy
+		article = Article.find(params[:id])
+		flash.notice = "article'#{article.title}' Destroyed"
+		article.destroy
+		# redirect_to action: "index"
+		redirect_to articles_path
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		flash.notice = "article'#{@article.title}' Updated"
+
+		redirect_to article_path(@article)
 	end
 end
--- blogger/app/controllers/untitled.sh
+++ blogger/app/controllers/untitled.sh
@@ -11,4 +11,10 @@ bin/rails generate controller articles # generates controller file
 	-> edit app/controllers/article_controller.rb to add an index method
 
 touch app/views/articles/index.html.erb
+	-> add some markup
+
+subl app/controllers/articles_controller.rb
+	-> add a show method
+
+touch app/views/articles/show.html.erb
 	-> add some markup 
